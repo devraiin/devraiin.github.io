aaa0500eef58414262f2f8d9a1ec246fa8ffde70
rainAkhi <150291133+devraiin@users.noreply.github.com>
2025-04-03 04:58:10 +0200

    ip grabber

--- js/typingAnimation.js
+++ js/typingAnimation.js
@@ -1,6 +1,6 @@
 // Custom the typed strings array based on your roles
 var typed = new Typed("#role", {
-  strings: ["a Java Developer", "a Python developer", "a Minecraft Plugin developer"],
+  strings: ["a Java Developer", "a Rust Developer", "a Minecraft Plugin developer"],
   typeSpeed: 80,
   backSpeed: 80,
   loop: true,
